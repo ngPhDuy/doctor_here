eb3bf5cd16429e7d9be46071c48daa1bdab87456
ngPhDuy <duy.nguyen9102003@hcmut.edu.vn>
2025-02-25 11:48:34 +0700

    ..

--- services/account.service.js
+++ services/account.service.js
@@ -5,10 +5,13 @@ exports.changePassword = async (username, password, newPassword) => {
     const account = await Account.findOne({
         where: {
             ten_dang_nhap: username,
-            mat_khau: password,
         },
     });
     if (!account) {
+        return false;
+    }
+    const match = await bcrypt.compare(password, account.mat_khau);
+    if (!match) {
         return false;
     }
     newPassword = await bcrypt.hash(newPassword, 10);
