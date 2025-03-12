b9f4a5f1e14e3abe0a4bf50e87c5109591f7fff3
ngPhDuy <duy.nguyen9102003@hcmut.edu.vn>
2025-03-12 13:11:19 +0700

    phan quyen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import DoctorListComponent from "./components/AdminDoctor/DoctorList";
@@ -16,59 +21,187 @@ import PatientDetailInfo from "./components/DoctorPatient/PatientDetailInfo";
 import PatientDetailHistory from "./components/DoctorPatient/PatientDetailHistory";
 import DoctorSettingInfo from "./components/DoctorSetting/DoctorSettingInfo";
 import DoctorRequest from "./components/DoctorSetting/DoctorRequest";
+import LoginComponent from "./pages/Login";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const role = localStorage.getItem("role");
+  if (!role) {
+    // Nếu không có role, chuyển hướng về trang đăng nhập
+    return <Navigate to="/login" />;
+  }
+  return <>{children}</>;
+};
+
 const App: React.FC = () => {
+  const role = localStorage.getItem("role"); // Kiểm tra role đã lưu trong localStorage
+
+  console.log(role + " from App Component");
+
   return (
     <Router>
-      <div className="App flex flex-col h-screen">
-        <Header />
-        <div className="flex flex-1">
-          <Sidebar />
-          <div className="w-full p-4">
-            <Routes>
-              <Route path="/" element={<DoctorListComponent />} />
-              <Route path="/doctorDetail/:id" element={<DoctorInfor />} />
-              <Route path="/patientList" element={<PatientListAdminComponent />} />
-              <Route path="/patientDetail/:id" element={<PatientDetail />} />
-              <Route path="/oldRequests" element={<OldRequestComponent />} />
-              <Route path="/newRequests" element={<NewRequestComponent />} />
-              <Route
-                path="/newRequestDetail/:requestId/:doctorId"
-                element={<NewRequestDetail />}
-              />
-              <Route
-                path="/historyList"
-                element={<HistoryListComponent />}
-              />
-              <Route
-                path="/historyDetail/:id"
-                element={<HistoryDetailComponent />}
-              />
-              <Route
-                path="/patientListDoctor"
-                element={<PatientListDoctorComponent />}
-              />
-              <Route
-                path="/patientInfoDoctor/:id"
-                element={<PatientDetailInfo />}
-              />
-              <Route
-                path="/patientDetailHistory/:patientId"
-                element={<PatientDetailHistory />}
-              />
-              <Route
-                path="/doctorSettingInfo"
-                element={<DoctorSettingInfo />}
-              />
-              <Route
-                path="/doctorRequest"
-                element={<DoctorRequest />}
-              />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        {/* Route cho trang đăng nhập */}
+        <Route path="/login" element={<LoginComponent />} />
+
+        {/* Các route yêu cầu Header và Sidebar */}
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <div className="App flex flex-col h-screen">
+                <Header />
+                <div className="flex flex-1">
+                  <Sidebar />
+                  <div className="w-full p-4">
+                    <Routes>
+                      {/* Routes dành cho Admin */}
+                      {role === "qtv" && (
+                        <>
+                          <Route path="/" element={<DoctorListComponent />} />
+                          <Route
+                            path="/doctorDetail/:id"
+                            element={<DoctorInfor />}
+                          />
+                          <Route
+                            path="/patientList"
+                            element={<PatientListAdminComponent />}
+                          />
+                          <Route
+                            path="/patientDetail/:id"
+                            element={<PatientDetail />}
+                          />
+                          <Route
+                            path="/oldRequests"
+                            element={<OldRequestComponent />}
+                          />
+                          <Route
+                            path="/newRequests"
+                            element={<NewRequestComponent />}
+                          />
+                          <Route
+                            path="/newRequestDetail/:requestId/:doctorId"
+                            element={<NewRequestDetail />}
+                          />
+                        </>
+                      )}
+
+                      {/* Routes dành cho Doctor */}
+                      {role === "bs" && (
+                        <>
+                          <Route
+                            path="/historyList"
+                            element={<HistoryListComponent />}
+                          />
+                          <Route
+                            path="/historyDetail/:id"
+                            element={<HistoryDetailComponent />}
+                          />
+                          <Route
+                            path="/patientListDoctor"
+                            element={<PatientListDoctorComponent />}
+                          />
+                          <Route
+                            path="/patientInfoDoctor/:id"
+                            element={<PatientDetailInfo />}
+                          />
+                          <Route
+                            path="/patientDetailHistory/:patientId"
+                            element={<PatientDetailHistory />}
+                          />
+                          <Route
+                            path="/doctorSettingInfo"
+                            element={<DoctorSettingInfo />}
+                          />
+                          <Route
+                            path="/doctorRequest"
+                            element={<DoctorRequest />}
+                          />
+                        </>
+                      )}
+                    </Routes>
+                  </div>
+                </div>
+              </div>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
+
+// const App: React.FC = () => {
+//   const role = localStorage.getItem("role");
+
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Trang đăng nhập */}
+//         <Route path="/login" element={<LoginComponent />} />
+
+//         {/* Các route yêu cầu Header và Sidebar */}
+//         <Route
+//           path="/*"
+//           element={
+//             role ? (
+//               <div className="App flex flex-col h-screen">
+//                 <Header />
+//                 <div className="flex flex-1">
+//                   <Sidebar />
+//                   <div className="w-full p-4">
+//                     <Routes>
+//                       {/* Routes dựa trên role */}
+//                       {role === "admin" ? (
+//                         <Route path="/" element={<DoctorListComponent />} />
+//                       ) : role === "doctor" ? (
+//                         <Route
+//                           path="/historyList"
+//                           element={<HistoryListComponent />}
+//                         />
+//                       ) : (
+//                         <Route path="/" element={<LoginComponent />} />
+//                       )}
+//                     </Routes>
+//                   </div>
+//                 </div>
+//               </div>
+//             ) : (
+//               <LoginComponent />
+//             )
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+
+// return (
+//   <Router>
+//     <Routes>
+//       {/* Route for Login (no Header, no Sidebar) */}
+//       <Route path="/login" element={<LoginComponent />} />
+
+//       {/* Routes that require Header and Sidebar */}
+//       <Route
+//         path="/*"
+//         element={
+//           <div className="App flex flex-col h-screen">
+//             <Header />
+//             <div className="flex flex-1">
+//               <Sidebar />
+//               <div className="w-full p-4">
+//                 <Routes>
+//                   {/* Route for Admin */}
+
+//                   {/* Route for Doctor */}
+
+//                 </Routes>
+//               </div>
+//             </div>
+//           </div>
+//         }
+//       />
+//     </Routes>
+//   </Router>
+// );
 
 export default App;
--- client/src/components/DoctorHistory/HistoryDetail.tsx
+++ client/src/components/DoctorHistory/HistoryDetail.tsx
@@ -47,7 +47,6 @@ interface Appointment {
   hinh_anh_bo_sung: string[];
 }
 
-  
 const defaultAppointment: Appointment = {
   id: 0,
   van_ban_bo_sung: "",
@@ -94,50 +93,57 @@ const defaultAppointment: Appointment = {
   hinh_anh_bo_sung: [],
 };
 
-
 const HistoryDetailComponent: React.FC = () => {
   const navigate = useNavigate();
-  const [appointment, setAppointment] = useState<Appointment>(defaultAppointment);
+  const [appointment, setAppointment] =
+    useState<Appointment>(defaultAppointment);
   const { id } = useParams<{ id: string }>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
   const [message, setMessage] = useState<string | null>(null);
-  const [messageType, setMessageType] = useState<"success" | "error" | null>(null);
+  const [messageType, setMessageType] = useState<"success" | "error" | null>(
+    null
+  );
 
   const formatTime = (isoString: string): string => {
-    if (!isoString) return "00:00"; 
-  
+    if (!isoString) return "00:00";
+
     const date = new Date(isoString);
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
-  
+
     return `${hours}:${minutes}`.trim();
   };
-  
-  
+
   const formatDate = (isoString: string): string => {
     const date = new Date(isoString);
-    return date.toLocaleDateString("vi-VN", { day: "2-digit", month: "2-digit", year: "numeric" });
+    return date.toLocaleDateString("vi-VN", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
   };
 
   const formatDateTime = (isoString: string): string => {
     const date = new Date(isoString);
-    
+
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
-    
+
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear();
-  
+
     return `${hours}:${minutes} ${day}/${month}/${year}`;
   };
-  
+
   const fetchAppointment = async () => {
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/appointment/detail?appointmentID= ${id}`
+        `${
+          import.meta.env.VITE_API_BASE_URL
+        }/api/appointment/detail?appointmentID= ${id}`
       );
       if (!response.ok) {
         throw new Error("Không thể tải danh sách lịch sử cuộc hẹn.");
@@ -154,7 +160,7 @@ const HistoryDetailComponent: React.FC = () => {
         ma_bac_si: data.ma_bac_si,
         ma_benh_nhan_dat_hen: data.ma_benh_nhan_dat_hen,
         id_gio_hen: data.id_gio_hen,
-      
+
         // Thông tin bệnh nhân
         benh_nhan_cccd: data.Benh_nhan.cccd,
         benh_nhan_dan_toc: data.Benh_nhan.dan_toc,
@@ -163,7 +169,7 @@ const HistoryDetailComponent: React.FC = () => {
         benh_nhan_quoc_tich: data.Benh_nhan.quoc_tich,
         benh_nhan_dia_chi: data.Benh_nhan.dia_chi,
         benh_nhan_ma_benh_nhan: data.Benh_nhan.ma_benh_nhan,
-      
+
         // Thông tin người dùng của bệnh nhân
         benh_nhan_ten_dang_nhap: data.Benh_nhan.Nguoi_dung.ten_dang_nhap,
         benh_nhan_email: data.Benh_nhan.Nguoi_dung.email,
@@ -172,7 +178,7 @@ const HistoryDetailComponent: React.FC = () => {
         benh_nhan_gioi_tinh: data.Benh_nhan.Nguoi_dung.gioi_tinh,
         benh_nhan_phan_loai: data.Benh_nhan.Nguoi_dung.phan_loai,
         benh_nhan_ho_va_ten: data.Benh_nhan.Nguoi_dung.ho_va_ten,
-      
+
         // Thông tin bác sĩ
         bac_si_ngay_vao_nghe: data.Bac_si.ngay_vao_nghe,
         bac_si_trinh_do_hoc_van: data.Bac_si.trinh_do_hoc_van,
@@ -180,16 +186,20 @@ const HistoryDetailComponent: React.FC = () => {
         bac_si_dia_chi_pk: data.Bac_si.dia_chi_pk,
         bac_si_ma_bac_si: data.Bac_si.ma_bac_si,
         bac_si_chuyen_khoa: data.Bac_si.chuyen_khoa,
-      
+
         // Thông tin giờ hẹn
         gio_hen_thoi_diem_bat_dau: data.Gio_hen.thoi_diem_bat_dau,
         gio_hen_thoi_diem_ket_thuc: data.Gio_hen.thoi_diem_ket_thuc,
         gio_hen_ngay_lam_viec: data.Gio_hen.ngay_lam_viec,
         gio_hen_available: data.Gio_hen.available,
         gio_hen_id_ca_lam_viec: data.Gio_hen.id_ca_lam_viec,
-        ca_lam_viec_lam_viec_onl: data.Gio_hen.Ca_lam_viec_trong_tuan.lam_viec_onl,
-        hinh_anh_bo_sung: data.Hinh_anh_bo_sung_cuoc_hen?.map((img: { url: string }) => img.url) || [],
-      };      
+        ca_lam_viec_lam_viec_onl:
+          data.Gio_hen.Ca_lam_viec_trong_tuan.lam_viec_onl,
+        hinh_anh_bo_sung:
+          data.Hinh_anh_bo_sung_cuoc_hen?.map(
+            (img: { url: string }) => img.url
+          ) || [],
+      };
       console.log(flatAppointment);
       setAppointment(flatAppointment);
     } catch (err) {
@@ -240,7 +250,6 @@ const HistoryDetailComponent: React.FC = () => {
     }
   };
 
-
   const calculateAge = (dob: string): number => {
     const birthDate = new Date(dob);
     const today = new Date();
@@ -263,7 +272,10 @@ const HistoryDetailComponent: React.FC = () => {
     <div className="h-full bg-gray-50">
       {/* Header */}
       <div className="flex items-center mb-4 bg-white rounded-lg shadow-md">
-        <div className="p-3 cursor-pointer" onClick={() => navigate("/historyList")}>
+        <div
+          className="p-3 cursor-pointer"
+          onClick={() => navigate("/historyList")}
+        >
           <svg
             width="24"
             height="24"
@@ -345,136 +357,149 @@ const HistoryDetailComponent: React.FC = () => {
             <h2 className="text-lg font-semibold ml-4">Thông tin chi tiết</h2>
             <span
               className={`px-3 py-2 text-sm font-medium rounded-full ml-auto capitalize ${
-                  appointment.trang_thai === "Đang chờ"
-                      ? "bg-blue-200 text-blue-700"
-                      : appointment.trang_thai === "Hoàn thành"
-                      ? "bg-green-200 text-green-700"
-                      : "bg-red-200 text-red-700"
+                appointment.trang_thai === "Đang chờ"
+                  ? "bg-blue-200 text-blue-700"
+                  : appointment.trang_thai === "Hoàn thành"
+                  ? "bg-green-200 text-green-700"
+                  : "bg-red-200 text-red-700"
               }`}
             >
               {appointment.trang_thai}
             </span>
           </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
               <label className="text-sm font-medium">Mã đơn hẹn</label>
               <input
-                  className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-                  value={appointment.ma_benh_nhan_dat_hen}
-                  disabled
+                className="w-full px-3 py-2 border rounded-lg bg-gray-100"
+                value={appointment.ma_benh_nhan_dat_hen}
+                disabled
               />
-          </div>
-          <div>
+            </div>
+            <div>
               <label className="text-sm font-medium">Tạo lúc</label>
               <input
-                  className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-                  value={formatDateTime(appointment.thoi_diem_tao)}
-                  disabled
+                className="w-full px-3 py-2 border rounded-lg bg-gray-100"
+                value={formatDateTime(appointment.thoi_diem_tao)}
+                disabled
               />
-          </div>
+            </div>
 
-          <div>
+            <div>
               <label className="text-sm font-medium">Hình thức</label>
               <input
-                  className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-                  value={appointment.ca_lam_viec_lam_viec_onl ? "Online" : "Offline"}
-                  disabled
-              />
-          </div>
-          <div>
-            <label className="text-sm font-medium">Thời điểm hẹn</label>
-            <input
                 className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-                value={`${formatTime(appointment.gio_hen_thoi_diem_bat_dau)} - ${formatTime(appointment.gio_hen_thoi_diem_ket_thuc)} ${formatDate(appointment.gio_hen_ngay_lam_viec)}`}
+                value={
+                  appointment.ca_lam_viec_lam_viec_onl ? "Online" : "Offline"
+                }
                 disabled
-            />
-          </div>
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium">Thời điểm hẹn</label>
+              <input
+                className="w-full px-3 py-2 border rounded-lg bg-gray-100"
+                value={`${formatTime(
+                  appointment.gio_hen_thoi_diem_bat_dau
+                )} - ${formatTime(
+                  appointment.gio_hen_thoi_diem_ket_thuc
+                )} ${formatDate(appointment.gio_hen_ngay_lam_viec)}`}
+                disabled
+              />
+            </div>
 
-
-          <div className="col-span-2">
+            <div className="col-span-2">
               <label className="text-sm font-medium">Địa chỉ</label>
               <input
-                  className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-                  value={appointment.bac_si_dia_chi_pk}
-                  disabled
+                className="w-full px-3 py-2 border rounded-lg bg-gray-100"
+                value={appointment.bac_si_dia_chi_pk}
+                disabled
               />
+            </div>
           </div>
-        </div>
-        {/* Văn bản bổ sung */}
-        <div className="mt-2">
+          {/* Văn bản bổ sung */}
+          <div className="mt-2">
             <label className="text-sm font-medium">Văn bản bổ sung</label>
             <textarea
-            className="w-full px-3 py-2 border rounded-lg bg-gray-100"
-            rows={3}
-            value={appointment.van_ban_bo_sung}
-            disabled
+              className="w-full px-3 py-2 border rounded-lg bg-gray-100"
+              rows={3}
+              value={appointment.van_ban_bo_sung}
+              disabled
             />
-        </div>
+          </div>
 
-        {/* File đính kèm */}
-        <div className="mt-2">
+          {/* File đính kèm */}
+          <div className="mt-2">
             <label className="text-sm font-medium">Hình ảnh bổ sung</label>
             <div className="w-full px-3 py-2 border rounded-lg bg-gray-100">
-                {appointment.hinh_anh_bo_sung && appointment.hinh_anh_bo_sung.length > 0 ? (
-                    appointment.hinh_anh_bo_sung.map((file: string, index: number) => {
-                        // Kiểm tra nếu file là ảnh (đuôi .png, .jpg, .jpeg, .gif)
-                        const isImage = /\.(jpeg|jpg|png|gif)$/i.test(file);
+              {appointment.hinh_anh_bo_sung &&
+              appointment.hinh_anh_bo_sung.length > 0 ? (
+                appointment.hinh_anh_bo_sung.map(
+                  (file: string, index: number) => {
+                    // Kiểm tra nếu file là ảnh (đuôi .png, .jpg, .jpeg, .gif)
+                    const isImage = /\.(jpeg|jpg|png|gif)$/i.test(file);
 
-                        return (
-                            <div key={index}>
-                                <a
-                                    href={file}
-                                    className="text-blue-500 underline block"
-                                    target={isImage ? "_blank" : ""}
-                                    rel="noopener noreferrer"
-                                    download={!isImage} // Chỉ tải xuống nếu không phải ảnh
-                                >
-                                    {file}
-                                </a>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <p className="text-gray-500">Không có hình ảnh bổ sung</p>
-                )}
+                    return (
+                      <div key={index}>
+                        <a
+                          href={file}
+                          className="text-blue-500 underline block"
+                          target={isImage ? "_blank" : ""}
+                          rel="noopener noreferrer"
+                          download={!isImage} // Chỉ tải xuống nếu không phải ảnh
+                        >
+                          {
+                            //Tách tên file từ URL
+                            file.split("/").pop()
+                          }
+                        </a>
+                      </div>
+                    );
+                  }
+                )
+              ) : (
+                <p className="text-gray-500">Không có hình ảnh bổ sung</p>
+              )}
             </div>
-        </div>
-
-      <div>
-        {/* Hiển thị thông báo */}
-        {message && (
-          <div
-            className={`fixed top-5 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded-lg text-white ${
-              messageType === "success" ? "bg-green-500" : "bg-red-500"
-            } transition-opacity duration-500`}
-          >
-            {message}
           </div>
-        )}
 
-        {/* Nút chức năng */}
-        {appointment.trang_thai?.toLowerCase() === "đang chờ" ? (
-          <div className="flex justify-center gap-4 mt-6">
-            <button
-              onClick={() => handleUpdateStatus("Đang chờ")}
-              className="px-6 py-2 text-white bg-blue-800 rounded-lg font-medium hover:bg-blue-900 transition duration-200"
-            >
-              Gửi kết quả
-            </button>
-            <button
-              onClick={() => handleUpdateStatus("Hoàn thành")}
-              className="px-6 py-2 text-white bg-green-600 rounded-lg font-medium hover:bg-green-700 transition duration-200"
-            >
-              Hoàn thành
-            </button>
-            </div>
+          <div>
+            {/* Hiển thị thông báo */}
+            {message && (
+              <div
+                className={`fixed top-5 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded-lg text-white ${
+                  messageType === "success" ? "bg-green-500" : "bg-red-500"
+                } transition-opacity duration-500`}
+              >
+                {message}
+              </div>
+            )}
+
+            {/* Nút chức năng */}
+            {appointment.trang_thai?.toLowerCase() === "đang chờ" ? (
+              <div className="flex justify-center gap-4 mt-6">
+                <button
+                  onClick={() => handleUpdateStatus("Đang chờ")}
+                  className="px-6 py-2 text-white bg-blue-800 rounded-lg font-medium hover:bg-blue-900 transition duration-200"
+                >
+                  Gửi kết quả
+                </button>
+                <button
+                  onClick={() => handleUpdateStatus("Hoàn thành")}
+                  className="px-6 py-2 text-white bg-green-600 rounded-lg font-medium hover:bg-green-700 transition duration-200"
+                >
+                  Hoàn thành
+                </button>
+              </div>
             ) : appointment.trang_thai?.toLowerCase() === "hoàn thành" ? (
               <div className="mt-6 text-center">
-                <button className="text-blue-500 underline">Bác sĩ kê đơn</button>
+                <button className="text-blue-500 underline">
+                  Bác sĩ kê đơn
+                </button>
               </div>
             ) : null}
           </div>
-      </div>
+        </div>
 
         {/* Thông tin cá nhân bên phải */}
         <div className="w-1/3 bg-white p-6 rounded-lg shadow-md">
@@ -485,7 +510,8 @@ const HistoryDetailComponent: React.FC = () => {
               className="w-20 h-20 rounded-full mb-4"
             />
             <p className="text-base font-semibold text-red-600">
-              {appointment.benh_nhan_ho_va_ten} ({appointment.benh_nhan_ma_benh_nhan})
+              {appointment.benh_nhan_ho_va_ten} (
+              {appointment.benh_nhan_ma_benh_nhan})
             </p>
             <p className="text-gray-600">
               {calculateAge(appointment.benh_nhan_ngay_sinh)} tuổi,{" "}
@@ -498,9 +524,12 @@ const HistoryDetailComponent: React.FC = () => {
             {[
               { label: "Email", value: appointment.benh_nhan_email },
               { label: "SĐT", value: appointment.benh_nhan_sdt },
-              { label: "Ngày sinh", value: appointment.benh_nhan_ngay_sinh},
+              { label: "Ngày sinh", value: appointment.benh_nhan_ngay_sinh },
               { label: "Địa chỉ", value: appointment.benh_nhan_dia_chi },
-              { label: "Tiền sử bệnh lý", value: appointment.benh_nhan_tien_su_benh},
+              {
+                label: "Tiền sử bệnh lý",
+                value: appointment.benh_nhan_tien_su_benh,
+              },
             ].map((info) => (
               <div className="mb-3" key={info.label}>
                 <p className="text-base font-medium">{info.label}</p>
